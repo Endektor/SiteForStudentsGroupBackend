dc4a5aa42b75361202e0001cc53315eddd81dc0e
Endektor <v.nikolaev55555@mail.ru>
2021-02-28 03:14:53 +0300

    configured celery

--- mail_app/apps.py
+++ mail_app/apps.py
@@ -16,10 +16,14 @@ class MailAppConfig(AppConfig):
             print('Mail check ' + datetime.datetime.today().strftime("%H.%M.%S"), flush=True)
             service = Service()
             service.get_mails()
-            sleep(60*5)
+            sleep(60 * 5)
 
     def ready(self):
+        mail_for_debug = False
         if os.environ.get('RUN_MAIN', None) != 'true' and \
-                not os.environ.get('Mail_env', None) == 'true':
+                not os.environ.get('Mail_env', None) == 'true' and \
+                mail_for_debug:
+            self.start_mail_service(self)
+            os.system('python manage.py celeryd -l INFO -B')
             p = Process(target=MailAppConfig.start_mail_service)
             p.start()
