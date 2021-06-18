4e47591f736709e9ba0ec1162d43818cb3f5c6fd
root <root@80-78-240-154.cloudvps.regruhosting.ru>
2021-06-19 00:27:42 +0300

    bug fixed in requirements and custom_auth/models and db changes in settings

--- the_site/settings.py
+++ the_site/settings.py
@@ -1,6 +1,9 @@
 from datetime import timedelta
 import os
 from decouple import config
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -15,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['aficionadoleague.ru']
+ALLOWED_HOSTS = ['aficionadoleague.ru', 'backend']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
@@ -111,13 +114,16 @@ EMAIL_USE_TLS = True
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
+DATABASES = {
+    'default': env.db(),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -179,11 +185,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/backendstatic/'
 STATIC_ROOT = BASE_DIR + '/static'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend/build/static'),
-)
 
 
 MEDIA_URL = '/media/'
